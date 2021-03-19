c15b940ad2a042a5188e59266f27468ae1a541c0
Bui Manh Ha <buimanhha91@gmail.com>
2021-03-19 14:30:53 +0700

    update config oauth

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
+export const process_port = 9311;
+export const oauth_link = 'https://mobileapp.hoanglongclinic.vn';
 
 export const facebook = {
   clientID: '1179851765780596',
   clientSecret: '63068edb31264ab14d323e2a7827bc84',
-  callbackURL: 'http://4dd41c244a96.ngrok.io/auth/facebook/callback',
+  callbackURL: oauth_link + '/auth/facebook/callback',
   profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
 };
 
 export const google = {
   clientID: '1094164047378-515sc27ard4rj9b59212jti1kmujrfko.apps.googleusercontent.com',
   clientSecret: 'P3w25npm-R9VMg7qXRYJbhOX',
-  callbackURL: 'http://4dd41c244a96.ngrok.io/auth/google/callback',
+  callbackURL: oauth_link + '/auth/google/callback',
 };
