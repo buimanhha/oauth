d9f7dc43d4a0a1b0916d705f588d3c16bedc50e2
Bui Manh Ha <buimanhha91@gmail.com>
2021-03-19 07:46:09 +0700

    start

---
+++ config.js
@@ -0,0 +1,13 @@
+
+export const facebook = {
+  clientID: '1179851765780596',
+  clientSecret: '63068edb31264ab14d323e2a7827bc84',
+  callbackURL: 'http://4dd41c244a96.ngrok.io/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
+
+export const google = {
+  clientID: '1094164047378-515sc27ard4rj9b59212jti1kmujrfko.apps.googleusercontent.com',
+  clientSecret: 'P3w25npm-R9VMg7qXRYJbhOX',
+  callbackURL: 'http://4dd41c244a96.ngrok.io/auth/google/callback',
+};
